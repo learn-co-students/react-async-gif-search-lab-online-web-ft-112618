2f1ff9e4765197dc73fac4a7312b128199d9886b
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-15 17:44:44 -0400

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -4,9 +4,14 @@ export default class GifSearch extends Component {
   state = { query: '' }
 
   handleChange = (e) => this.setState({ query: e.target.value })
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.fetchGifs(this.state.query);
+  }
   render() {
     return(
-      <form onSubmit={this.props.handleSubmit}>
+      <form onSubmit={this.handleSubmit}>
         <input type="text" id="search" onChange={this.handleChange} value={this.state.query} placeholder="Type your search here..."></input>
       </form>
     )
--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -3,29 +3,27 @@ import GifList from '../components/GifList'
 import GifSearch from '../components/GifSearch'
 
 export default class GifListContainer extends Component {
-  state = { gifs: [], query: 'dogs' };
+  state = { gifs: [] };
 
   componentDidMount() {
-    let uri = 'http://api.giphy.com/v1/gifs/search?q=' + this.state.query + '&api_key=dc6zaTOxFJmzC&rating=g'
-    fetch(uri).then(resp => resp.json()).then(json => {
-        let array = json.data
-        for (let gif of array) {
-          this.setState({
-            gifs: [...this.state.gifs, gif.images.original.url]
-          })
-        }
-      }
-    )
+    this.fetchGifs('kittens')
   }
 
-  handleSubmit() {
-
+  fetchGifs = (query) => {
+    let uri = 'http://api.giphy.com/v1/gifs/search?q=' + query + '&api_key=dc6zaTOxFJmzC&rating=g'
+    fetch(uri).then(resp => resp.json()).then(json => {
+      let array = json.data
+      this.setState({
+        gifs: [array[0].images.original.url, array[1].images.original.url, array[2].images.original.url]
+        })
+      }
+    )
   }
 
   render() {
     return(
       <div>
-        <GifSearch handleSubmit={this.handleSubmit}/>
+        <GifSearch fetchGifs={this.fetchGifs}/>
         <GifList gifs={this.state.gifs} />
       </div>
     )
